ed34b85de2f3f4f70b98b6d74f61aa1d5bbff885
Shin-ichi Takemura <shin_takemura@outlook.jp>
2021-02-24 17:12:27 +0900

    Initial Commit

---
+++ MinimalQuizApp3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinimalQuizApp3
+//
+//  Created by 竹村信一 on 2021/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
